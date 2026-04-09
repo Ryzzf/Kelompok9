f26eedc09bbce7c04d43aac5f0958176f956f9f8
Ryzzf <ranggaeizy18@gmail.com>
2026-04-09 16:50:51 +0700

    saya menambahkan routes untuk web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/settings', function () {
+    return view('settings');
 });
